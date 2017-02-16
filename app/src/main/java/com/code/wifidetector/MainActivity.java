39a9fae65b8a4980d7d9e7f28bbe28ad33b8bad8
karan <karanjain98>
2017-02-16 21:07:36 +0530

    all files

---
+++ app/src/main/java/com/code/wifidetector/MainActivity.java
@@ -0,0 +1,100 @@
+package com.code.wifidetector;
+
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String TAG="mainacta";
+    List<ScanResult> wifiList;
+    RecyclerView rv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        rv= (RecyclerView) findViewById(R.id.rv);
+        WIFIAdapter wa=new WIFIAdapter();
+        LinearLayoutManager la=new LinearLayoutManager(this);
+
+
+// Level of a Scan Result
+        wifiList = wifiManager.getScanResults();
+        for (ScanResult scanResult : wifiList) {
+            int level = WifiManager.calculateSignalLevel(scanResult.level,1000);
+            String a=wifiList.toString();
+            Log.d(TAG, "onCreate: "+level+"out of 5"+wifiList.toString());
+        }
+
+// Level of current connection
+        int rssi = wifiManager.getConnectionInfo().getRssi();
+        int level = WifiManager.calculateSignalLevel(rssi, 5);
+        rv.setAdapter(wa);
+        rv.setLayoutManager(la);
+
+
+    }
+    public class WifiHolder extends RecyclerView.ViewHolder {
+
+        public WifiHolder(View itemView) {
+            super(itemView);
+            view =itemView;
+            t= (TextView) itemView.findViewById(R.id.name);
+        }
+        View view;
+        TextView t;
+
+
+    }
+    public class WIFIAdapter extends RecyclerView.Adapter<WifiHolder>{
+
+
+        @Override
+        public WifiHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater li= getLayoutInflater();
+            View convertview=null;
+            convertview=li.inflate(R.layout.list_item,parent,false);
+            WifiHolder wv=new WifiHolder(convertview);
+            return wv;
+
+        }
+
+        @Override
+        public void onBindViewHolder(WifiHolder holder, final int position) {
+
+            holder.t.setText(wifiList.get(position).SSID.toString());
+            holder.view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i= new Intent(MainActivity.this,SpecificWifiActivity.class);
+                    i.putExtra("rfv",wifiList.get(position).BSSID.toString());
+                    i.putExtra("name",wifiList.get(position).SSID);
+                    startActivity(i);
+                }
+            });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return wifiList.size();
+        }
+    }
+}
---
+++ app/src/main/java/com/code/wifidetector/SpecificWifiActivity.java
@@ -0,0 +1,93 @@
+package com.code.wifidetector;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.GridLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+/**
+ * Created by karan jain on 16-02-2017.
+ */
+
+public class SpecificWifiActivity extends AppCompatActivity {
+    String name;
+    List<ScanResult> wifiList;
+    Intent i;
+    String wifiName;
+    TextView level2,name1;
+    public static final String TAG="specifiwifiactivity";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_specificwifi);
+         i=getIntent();
+        level2= (TextView) findViewById(R.id.level2);
+        name1= (TextView) findViewById(R.id.nameWifi);
+        name=i.getStringExtra("rfv");
+        wifiName=i.getStringExtra("name");
+
+        WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+        wifiList = wifiManager.getScanResults();
+        name1.setText(wifiName.toString());
+        for (ScanResult scanResult : wifiList) {
+            int level = WifiManager.calculateSignalLevel(scanResult.level,1000);
+            String a=wifiList.toString();
+            Log.d(TAG, "onCreate: "+level+"out of 5"+wifiList.toString());
+        }
+
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        //Note: Not using RSSI_CHANGED_ACTION because it never calls me back.
+        IntentFilter rssiFilter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+        this.registerReceiver(myRssiChangeReceiver, rssiFilter);
+
+        WifiManager wifiMan=(WifiManager) SpecificWifiActivity.this.getSystemService(Context.WIFI_SERVICE);
+        wifiMan.startScan();
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        this.unregisterReceiver(myRssiChangeReceiver);
+
+    }
+    /**
+     * Broadcast receiver to update
+     */
+    private BroadcastReceiver myRssiChangeReceiver
+            = new BroadcastReceiver(){
+        @Override
+        public void onReceive(Context arg0, Intent arg1) {
+            WifiManager wifiMan=(WifiManager)SpecificWifiActivity.this.getSystemService(Context.WIFI_SERVICE);
+            wifiMan.startScan();
+            int newRssi = wifiMan.getConnectionInfo().getRssi();
+            wifiList = wifiMan.getScanResults();
+            for (ScanResult scanResult : wifiList) {
+                //int level = WifiManager.calculateSignalLevel(scanResult.level,1000);
+
+               // Log.d(TAG, "onCreate: "+level+"out of 5"+wifiList.toString());
+                if(name.equals(scanResult.BSSID))
+                {  int level1 = WifiManager.calculateSignalLevel(scanResult.level,10);
+                   level2.setText(""+level1);
+                Log.d(TAG, "onReceive: "+ level1+"  "+scanResult.SSID);}
+            }
+
+
+        }};
+}
